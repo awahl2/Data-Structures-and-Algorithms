9d35f7794094666183b389f08f6e380f587b81ac
Anna Wahl <123816763+awahl2@users.noreply.github.com>
2025-09-03 14:02:28 -0400

    Update lab2.java

--- lab2/lab2.java
+++ lab2/lab2.java
@@ -3,34 +3,51 @@
 // September 5, 2025
 // calculates primes using Sieve of Eratosthenes
 // demonstrates command line arguments and methods
-// *** add answers to performance questions here ***
+
+// The max prime I could find using this was 9999991. It took awhile for it to load.
+
+// It finishes insanely faster when I don't print the numbers.
+
+// Mostly time limits finding larger numbers. I input 100000000, but it took too long. If I were only to output the last number, I would be able to do more.
 
 public class lab2 {
+    
+    public static void main(String[] args) {
 
-	public static void main(String[] args) 
-	{
-		int num = 0;
-		
-		//add code to get num from command line argument
-		if (args.length > 0)
-        {
-            int N = 0;
-            N = Integer.parseInt( args[0] );
-            System.out.println( "N = " + N);
+        int N = 0;
+
+        // Check if arguments are provided
+        if (args.length == 0) {
+            N = 1000;
+        } else {
+            N = Integer.parseInt(args[0]);
         }
-        else
-        {
-            System.out.println("Missing argument");
+
+        // setup array of booleans
+        boolean[] lights = new boolean[N + 1];
+
+        // set lights[0] and lights[1] to false
+        lights[0] = false;
+        lights[1] = false;
+
+        // set lights[2] to lights[N] to false
+        for (int i = 2; i <= N; i++) {
+            lights[i] = true;
         }
-        
-		//call showPrimes
-		showPrimes( num );
-		
-	}//main
-	
-	public static void showPrimes(int N)
-	{
-		//add code to display primes		
-	} //showPrimes
-	
-} //lab2
+
+        // mark off values
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            for (int j = i * i; j <= N; j += i) {
+                lights[j] = false;
+            }
+        }
+
+        // display results
+        for (int i = 2; i <= N; i++) {
+            if (lights[i] == true) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
